80691a191085382e952b9849ba3d32fa26649db8
fairu <fairuz.fuadi04@gmail.com>
2025-11-17 17:09:46 +0700

    day1

---
+++ day1.py
@@ -0,0 +1,23 @@
+# we can get an output from a pyhton language by using a print statement like below
+print("are u okay?")
+# we can also do some basic aritmatic operations like below
+print(3+4)
+# the different of the fist print stament and second first statement is the first statament using a string an we have to add "" or '' to make it a string while the second statement is using integers so we dont need to add "" or '' to it. if we want to try give "" on aritmatic operations it will give an output like this
+print("3"+"4")
+# it will give an output like this 34 it is called concatenation which is just joining two strings together. but when we use other aritmatic opertaions like minus etc.. it will give an error like below
+# print("3"-"4")
+# it will give an error that says unsupported operator '-' for stings.
+# so to avoid this error we have to convert the string to integer using int() function like below
+print(int("3")-int("4"))
+# now it will give an output like this -1
+# we can also use float() function to convert a string to float like below
+print(float("3.5")+float("4.2"))
+# it will give an output like this 7.7
+# and then have a some function like this
+print("add this number:", 5+3)
+print('januari',"februari","agustus", sep="-")
+print("fa: hey \nsa: haai")
+# etc.. you can try more things by yourself.
+# happy coding :)
+
+# variables and Data Types in Python
